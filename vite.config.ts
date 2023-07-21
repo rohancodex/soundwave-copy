f26566a1309f48371f767f6a3b9c24f0ade653a0
rohancodex <rohancodex@gmail.com>
2023-07-21 22:55:10 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,3 @@ export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
 });
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import path from 'path'
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, './src'),
-//     }
-//   },
-// })
